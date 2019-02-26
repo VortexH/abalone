3afdaf783e30fff5077d7a0e2157a5df35b6d543
Stephen Chu <stephen.chu530@gmail.com>
2019-02-26 15:43:31 -0800

    fix: changed post comment logic

--- web_dynamic/static/scripts/post-comment.js
+++ web_dynamic/static/scripts/post-comment.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
+    let feed = $('#message-feed');
     $('#submit-comment').click(function(){
         let newComment = $('textarea.new-comment');
         let textVal = newComment.val();
+        let time = new Date();
+        let time_db = String(time.toLocaleString());
+        let time_chat = time.toLocaleTimeString();
+        feed.stop().animate({ scrollTop: feed[0].scrollHeight}, 1000);
         newComment.val("");
         $('#message-feed ul').append(`<li class="loaded-comment"><span class="username">Username: </span><span class="time">3:25:21</span><br><span class="message">${textVal}</span></li>`);
-        console.log(textVal);
+        videoId = localStorage.getItem("video_id");
+        dict = {videoId: ['user_name', 'comment', 'time']}
+        // alert(dict.videoId)
+        $.ajax({
+          url: 'https://abalone.holberton.us/api/submit_comment',
+          dataType: 'json',
+          crossDomain: true,
+          type: 'post',
+          contentType: 'application/json',
+          data: JSON.stringify(dict),
+          success: function(data, status) {
+            alert(data) }
+        });
+
     });
     // Enable enter key to submit comment.
     $("textarea").keypress(function (e) {
