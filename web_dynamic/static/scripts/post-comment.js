1b833c35ade7be650e0a40f1b2fc8c4e736ee8cc
robertglatzel <robertglatzel@gmail.com>
2019-02-26 13:48:07 -0800

    Adding all work

--- web_dynamic/static/scripts/post-comment.js
+++ web_dynamic/static/scripts/post-comment.js
@@ -3,10 +3,12 @@ $(document).ready(function(){
     $('#submit-comment').click(function(){
         let newComment = $('textarea.new-comment');
         let textVal = newComment.val();
+        let time = new Date();
+        let time_db = time.toLocaleString();
+        let time_chat = time.toLocaleTimeString();
         feed.stop().animate({ scrollTop: feed[0].scrollHeight}, 1000);
         newComment.val("");
-        $('#message-feed ul').append(`<li class="loaded-comment"><span class="username">Username: </span><span class="time">3:25:21</span><br><span class="message">${textVal}</span></li>`);
-
+        $('#message-feed ul').append(`<li class="loaded-comment"><span class="username"><strong>Username:</strong> </span><span class="time">${time_chat}</span><br><span class="message">${textVal}</span></li>`);
     });
     // Enable enter key to submit comment.
     $("textarea").keypress(function (e) {
