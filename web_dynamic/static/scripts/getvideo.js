1b6416f12ef07caf8378b7733ea3370d06e88b4b
robertglatzel <robertglatzel@gmail.com>
2019-02-26 12:26:23 -0800

    Pushing updated files

---
+++ web_dynamic/static/scripts/getvideo.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('#submit-video').click(function(){
+        let searchText = $('#search-input');
+        let videoId = searchText.val();
+        searchText.val("");
+        console.log(videoId);
+        $.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&key=AIzaSyArpdRTVCbPHEpMBZdW2sl9btD5igbul0E&part=status', function (video) {
+        	try {
+        		if (video.items[0].status.embeddable) {
+        			$('.video-container').append(`<iframe
+width="640" height="360"src="https://www.youtube.com/embed/${videoId}">
+</iframe> `);
+        		} else {
+        			throw "bad";
+        		}
+        	}
+        	catch {
+        		alert("Invalid link");
+        	}
+        });
+    });
+    // Enable enter key to submit comment.
+    $('#search-bar').keypress(function (e) {
+        if(e.which == 13 && !e.shiftKey) {
+            $('#submit-video').click();
+            e.preventDefault();
+            return false;
+        }
+    });
+});
