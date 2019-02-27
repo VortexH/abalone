265ff59507adfba5be5ae74d6c66fd622dc5259f
dspham <dsvpham@gmail.com>
2019-02-27 00:35:45 +0000

    stats

--- web_dynamic/static/scripts/getvideo.js
+++ web_dynamic/static/scripts/getvideo.js
@@ -6,11 +6,19 @@ $(document).ready(function(){
         localStorage.setItem("video_id", videoId);
         searchText.val("");
         console.log(videoId);
-        $.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&key=AIzaSyArpdRTVCbPHEpMBZdW2sl9btD5igbul0E&part=status', function (video) {
+        $.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + videoId + '&key=AIzaSyArpdRTVCbPHEpMBZdW2sl9btD5igbul0E&part=status,statistics,snippet', function (video) {
         	try {
         		if (video.items[0].status.embeddable) { $('.video-container').append(`<iframe
 width="640" height="360"src="https://www.youtube.com/embed/${videoId}">
 </iframe> `);
+                    let likeCount = video.items[0].statistics.likeCount;
+                    let viewCount = video.items[0].statistics.viewCount;
+                    let title = video.items[0].snippet.title;
+                    let description = video.items[0].snippet.description;
+                    $('.stats_body').html(`<br />Title: ${title}<br /><br />`);
+                    $('.stats_body').append(`<strong>Description:</strong> ${description}<br /><br />`);
+                    $('.stats_body').append(`<b>Views:</b> ${viewCount}<br /><br />`);
+                    $('.stats_body').append(`<b>Likes:</b> ${likeCount}<br /><br />`);
               $.get('https://abalone.holberton.us/api/get_comments/' + String(videoId), function(status) {
               });
         		} else {
